73709403615c9f11c0e79d46f50c385fc78e580a
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-05 16:13:45 -0300

    Initial commit

---
+++ src/test/java/com/zupedu/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zupedu.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
