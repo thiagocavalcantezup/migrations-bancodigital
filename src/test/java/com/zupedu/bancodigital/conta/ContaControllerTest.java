78deefa2ef57d0b08540b2ea930a795a5d295698
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-05-09 17:22:29 -0300

    Add integration test for payment creation

---
+++ src/test/java/com/zupedu/bancodigital/conta/ContaControllerTest.java
@@ -0,0 +1,45 @@
+package com.zupedu.bancodigital.conta;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ContaControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void deveriaCadastrarUmPagamento() throws Exception {
+
+        String pagamentoRequestJson = "{\n" + "\"codigoDeBarras\": \"123456789\",\n"
+                + "\"valor\": 29.99,\n" + "\"codigoDeConfirmacao\": \"ABC123DEF456\"\n" + "}\n";
+
+        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+
+        mockMvc.perform(
+            post(ContaController.BASE_URI + "/1" + ContaController.PAGAMENTO_URI).contentType(
+                MediaType.APPLICATION_JSON
+            ).content(pagamentoRequestJson)
+        )
+               .andExpect(status().isCreated())
+               .andExpect(
+                   header().string(
+                       "location",
+                       baseUrl + ContaController.BASE_URI + "/1" + ContaController.PAGAMENTO_URI
+                               + "/1"
+                   )
+               );
+    }
+
+}
---
+++ src/test/resources/db/migration/V30220509164700__cadastrar_conta.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+  contas (
+    id,
+    documento_titular,
+    nome_titular,
+    abertura,
+    agencia,
+    numero,
+    saldo
+  )
+VALUES
+  (
+    DEFAULT,
+    '123.456.789-00',
+    'Thiago',
+    CURRENT_TIMESTAMP,
+    1,
+    12345,
+    23.45
+  )
